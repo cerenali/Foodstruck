59df902b17420a096f92baff051e0f36f61e621d
Alice <aliceren@seas.upenn.edu>
2015-01-17 17:49:02 -0500

    add checkout buttons

---
+++ FoodTruckCentral/CheckoutViewController.h
@@ -0,0 +1,15 @@
+//
+//  CheckoutViewController.h
+//  FoodTruckCentral
+//
+//  Created by Alice Ren on 1/17/15.
+//  Copyright (c) 2015 JAAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckoutViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *postmatesDeliveryButton;
+@property (weak, nonatomic) IBOutlet UIButton *googleWalletButton;
+
+@end
---
+++ FoodTruckCentral/CheckoutViewController.m
@@ -0,0 +1,37 @@
+//
+//  CheckoutViewController.m
+//  FoodTruckCentral
+//
+//  Created by Alice Ren on 1/17/15.
+//  Copyright (c) 2015 JAAA. All rights reserved.
+//
+
+#import "CheckoutViewController.h"
+
+@interface CheckoutViewController ()
+
+@end
+
+@implementation CheckoutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
