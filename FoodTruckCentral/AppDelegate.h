f8390ca891196a242f61e6bb2e05b23630ff4078
Alice <aliceren@seas.upenn.edu>
2015-01-16 20:02:11 -0500

    Initial Commit

---
+++ FoodTruckCentral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodTruckCentral
+//
+//  Created by Alice Ren on 1/16/15.
+//  Copyright (c) 2015 JAAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoodTruckCentral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoodTruckCentral
+//
+//  Created by Alice Ren on 1/16/15.
+//  Copyright (c) 2015 JAAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
